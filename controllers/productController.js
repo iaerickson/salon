8fdbde56b08b5a1c3c3e73b8b6acba31b410bfb1
Ian <ierickson25@gmail.com>
2020-12-21 02:09:27 -0600

    added some extra code into the routes and controller files

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,12 @@
 const db = require("../models");
 
-module.exports = {};
+module.exports = {
+	create: function (req, res) {
+		db.Product.create(req.body)
+			.then((data) => res.json(data))
+			.catch((err) => {
+				console.log(err);
+				res.status(400).json(err);
+			});
+	},
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,2 +1,10 @@
 const router = require("express").Router();
 const usersController = require("../../controllers/userController");
+
+router.route("login").post((req, res) => {});
+
+router.route("signup").post((req, res) => {
+	usersController.create(req, res);
+});
+
+module.exports = router;
