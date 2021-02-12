37dcbdfdee135253530ede085d5576425140fd38
Ian <ierickson25@gmail.com>
2021-02-11 21:00:29 -0600

    minor but crucial addition of making calendar unable to select days in the past

--- client/src/pages/Schedule.js
+++ client/src/pages/Schedule.js
@@ -3,19 +3,18 @@ import Container from "../components/Container";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
+const disabledRanges = [];
+
+function tileDisabled({ date, view }) {}
+
 function Schedule() {
 	const [value, onChange] = useState(new Date());
-	//State
-
-	//HandleChanges
-
-	//Render
 
 	return (
 		<div>
 			<Container>
 				<h1>Book Your Appointment Now</h1>
-				<Calendar onChange={onChange} value={value} />
+				<Calendar onChange={onChange} value={value} minDate={new Date()} />
 			</Container>
 		</div>
 	);
