24907b70f978c11c3023b9bd72de3222e5b5597c
Ian <ierickson25@gmail.com>
2021-03-04 20:11:01 -0600

    added link to schedule button on home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Container from "../components/Container";
 
 class Home extends Component {
@@ -15,9 +16,9 @@ class Home extends Component {
 					<h1>Salon Ultra</h1>
 
 					<p>Welcome</p>
-
-					{/* onClick Change/handle */}
-					<button>Schedule an Appointment</button>
+					<Link to='/schedule'>
+						<button>Schedule an Appointment</button>
+					</Link>
 				</Container>
 			</div>
 		);
