2cae7e772d4e8f5ca8cf8beb63264dad56d80c00
Ian <ierickson25@gmail.com>
2021-03-10 00:56:40 -0600

    got carousel working and sourced correctly to assets!

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -2,49 +2,36 @@ import React from "react";
 
 function Carousel() {
 	return (
-		<div
-			id='carouselExampleIndicators'
-			className='carousel slide'
-			data-ride='carousel'
-		>
-			<ol className='carousel-indicators'>
-				<li
-					data-target='#carouselExampleIndicators'
-					data-slide-to='0'
-					class='active'
-				></li>
-				<li data-target='#carouselExampleIndicators' data-slide-to='1'></li>
-				<li data-target='#carouselExampleIndicators' data-slide-to='2'></li>
-			</ol>
-			<div className='carousel-inner'>
-				<div className='carousel-item active'>
-					<img src='...' className='d-block w-100' alt='...' />
-				</div>
-				<div className='carousel-item'>
-					<img src='...' className='d-block w-100' alt='...' />
-				</div>
-				<div className='carousel-item'>
-					<img src='...' className='d-block w-100' alt='...' />
+		<div>
+			<div
+				id='carouselExampleSlidesOnly'
+				className='carousel slide'
+				data-ride='carousel'
+			>
+				<div className='carousel-inner'>
+					<div className='carousel-item active'>
+						<img
+							src={require("../../assets/ultimissimo.jpg")}
+							className='d-block w-100'
+							alt='Ultimissimo'
+						/>
+					</div>
+					<div className='carousel-item'>
+						<img
+							src={require("../../assets/trim.jpg")}
+							className='d-block w-100'
+							alt='Trim'
+						/>
+					</div>
+					<div className='carousel-item'>
+						<img
+							src={require("../../assets/moroccanOil.jpg")}
+							className='d-block w-100'
+							alt='Moroccan Oil'
+						/>
+					</div>
 				</div>
 			</div>
-			<a
-				className='carousel-control-prev'
-				href='#carouselExampleIndicators'
-				role='button'
-				data-slide='prev'
-			>
-				<span className='carousel-control-prev-icon' aria-hidden='true'></span>
-				<span className='sr-only'>Previous</span>
-			</a>
-			<a
-				className='carousel-control-next'
-				href='#carouselExampleIndicators'
-				role='button'
-				data-slide='next'
-			>
-				<span className='carousel-control-next-icon' aria-hidden='true'></span>
-				<span className='sr-only'>Next</span>
-			</a>
 		</div>
 	);
 }
--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Container from "../components/Container";
 import Carousel from "../components/Carousel";
 
 function Gallery() {
-	const [value, onChange] = useState(0);
-
 	return (
 		<div>
 			<Container>
 				<h1>Gallery</h1>
+				<Carousel />
 			</Container>
 		</div>
 	);
