734f20eed002aa1accea4536796055658b71fbb4
Ian <ierickson25@gmail.com>
2021-03-23 00:04:35 -0500

    added Modal component and input the example in the schedule page

--- client/src/pages/Schedule.js
+++ client/src/pages/Schedule.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Container from "../components/Container";
 import Calendar from "react-calendar";
+import Modal from "../components/Modal";
 import "react-calendar/dist/Calendar.css";
 
 function Schedule() {
@@ -11,6 +12,7 @@ function Schedule() {
 			<Container>
 				<h1>Book Your Appointment Now</h1>
 				<Calendar onChange={onChange} value={value} minDate={new Date()} />
+				<Modal />
 			</Container>
 		</div>
 	);
